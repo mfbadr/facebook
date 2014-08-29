b1917583ec9d00eb3be7b707741ddaa9251e626a
Mikey Badr <mfbadr@gmail.com>
2014-08-28 20:14:10 -0500

    refactored messaging

---
+++ app/models/message.js
@@ -0,0 +1,38 @@
+'use strict';
+
+var _      = require('lodash'),
+    Mongo  = require('mongodb');
+
+function Message(data){
+  this.from = data.from; //email
+  this.to = data.to; //oid
+  this.date = new Date();
+  this.body = data.body;
+  this.isRead = false;
+}
+
+Object.defineProperty(Message, 'collection', {
+  get: function(){return global.mongodb.collection('messages');}
+});
+
+
+Message.findById = function(id, cb){
+  var _id = Mongo.ObjectID(id);
+  Message.collection.findOne({_id:_id}, function(err, obj){
+    obj.isRead = true;
+    cb(err, _.create(Message.prototype, obj));
+  });
+};
+
+
+
+Message.all = function(cb){
+  Message.collection.find().toArray(cb);
+};
+
+Message.find = function(query, cb){
+  Message.collection.find(query).toArray(cb);
+};
+
+module.exports = Message;
+
